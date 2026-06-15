3597325473b7808b49a3a61c519a5a0276dcffaa
Shaurav Bhatt <bhattshaurav404@gmail.com>
2026-06-15 12:48:37 +0530

    Here's my first c++ code

---
+++ BASIC CPP/03_1st_CPP_CODE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>  
+// Ye computer ko batata hai ki input-output (print karna, read karna) use karna hai
+
+using namespace std;  
+// Isse hume "std::cout" likhne ki zarurat nahi padti, direct "cout" likh sakte hain
+
+int main() {  
+    // Yahi se program start hota hai
+
+    cout << "Hello Shaurav !!" << "\n";  
+    // Screen par message print karta hai
+
+    return 0;  
+    // Program ko safely end karta hai (0 means success)
+}
